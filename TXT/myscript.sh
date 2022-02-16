7a74ac8e43b5ea0c3cb19042838c31bd6bb03b1d
Dewangga Putra Sheradhien <dewa251202@gmail.com>
2022-02-16 20:37:58 +0700

    Do Assignment #1

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,7 +1,42 @@
 #!/bin/bash
+# Copyright (C) 2020-2022 Cicak Bin Kadal
 
-FILES="my*.txt my*.sh"
+if [[ -z "$1" ]] ; then
+	echo "No week specified"
+	exit 0
+fi
+
+WEEK="$1"
+
+# ATTN:
+# You new to set "REC2" with your own Public-Key Identity!
+# Check it out with "gpg --list-key"
+
+REC2="181BED3AF58F3030"
+REC1="63FB12B215403B20"
+FILES="my*.asc my*.txt my*.sh"
 SHA="SHA256SUM"
+RESDIR="$HOME/SP_RESULT/"
+
+[ -d $RESDIR ] || mkdir -p $RESDIR
+pushd $RESDIR
+for II in W?? ; do
+    [ -d $II ] || continue
+    TARFILE=my$II.tar.bz2
+    TARFASC=$TARFILE.asc
+    rm -vf $TARFILE $TARFASC
+    echo "tar cfj $TARFILE $II/"
+    tar cfj $TARFILE $II/
+    echo "gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE"
+    gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE
+done
+popd
+
+if [[ "$WEEK" != "00" ]] ; then
+    II="${RESDIR}myW$WEEK.tar.bz2.asc"
+    echo "Check and move $II..."
+    [ -f $II ] && mv -vf $II .
+fi
 
 echo "rm -f $SHA $SHA.asc"
 rm -f $SHA $SHA.asc
@@ -12,9 +47,16 @@ sha256sum $FILES > $SHA
 echo "sha256sum -c $SHA"
 sha256sum -c $SHA
 
-echo "gpg -o $SHA.asc -a -sb $SHA"
-gpg -o $SHA.asc -a -sb $SHA
+echo "gpg --output $SHA.asc --armor --sign --detach-sign $SHA"
+gpg --output $SHA.asc --armor --sign --detach-sign $SHA
 
 echo "gpg --verify $SHA.asc $SHA"
 gpg --verify $SHA.asc $SHA
 
+echo ""
+echo "==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ===="
+echo "==== ==== ==== ATTN: is this WEEK $WEEK ?? ==== ==== ==== ===="
+echo "==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ===="
+echo ""
+
+exit 0
